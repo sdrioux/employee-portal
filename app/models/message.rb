173f9f9e745b9a105a6c52c5c46bde8f1ed9b799
MakerSquare Student <students@themakersquare.com>
2013-08-02 13:57:30 -0500

    write tets for working message system

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,17 @@ class Message < ActiveRecord::Base
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
 
   scope :unread, -> { where(read: false) }
+
+  # Deliver this message from sender to recipient.
+  #
+  # sending - The user sending the message
+  # receiving - The user receiving the message.
+  #
+  # Returns the delivered Message.
+  def deliver!(sending: nil, receiving: nil)
+    raise ArgumentError, "both sender and receiver must be specified" unless sending && receiving
+    self.sender = sending
+    self.recipient = receiving
+    self.save
+  end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,26 +2,44 @@ require 'spec_helper'
 
 describe Message do
   describe '.deliver!' do
-    let(:message){ Message.new(body: 'hello, there!')}
+    let(:message) { Message.new(body: 'hello, there!')}
+    let(:sender) { mock_model(User) }
+    let(:recipient) { mock_model(User) }
+
 
     context 'when both the sender and recipient are specified' do
-      it 'should set the sender of the message'
+      before(:each) do
+        message.deliver! sending: sender, receiving: recipient
+      end
+      it 'should set the sender of the message' do
+        expect(message.sender).to eq(sender)
+      end
 
-      it 'should set the recipient of the message'
+      it 'should set the recipient of the message' do
+        expect(message.recipient).to eq(recipient)
+      end
 
-      it 'should persist the message to the database'
+      it 'should persist the message to the database' do
+        expect(message.persisted?).to be_true
+      end
     end
 
     context 'when neither the sender nor recipient is specified' do
-      it 'should raise an error'
+      it 'should raise an error' do
+        expect { message.deliver! }.to raise_error ArgumentError
+      end
     end
 
     context 'when only the sender is specified' do
-      it 'should raise an error'
+      it 'should raise an error' do
+        expect { message.deliver! sending: sender }.to raise_error ArgumentError
+      end
     end
 
     context 'when only the recipient is specified' do
-      it 'should raise an error'
+      it 'should raise an error' do
+        expect { message.deliver! receiving: recipient }.to raise_error ArgumentError
+      end
     end
   end
 end
