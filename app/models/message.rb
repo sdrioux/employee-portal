fac7e8f0d81bd1614207c06027c05346cf1b3d6d
Matt Buck <github@matthewbuck.com>
2013-08-02 10:40:17 -0500

    Properly handle error cases.

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,14 @@ class Message < ActiveRecord::Base
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
 
   scope :unread, -> { where(read: false) }
+
+  # Deliver this message from sender to recipient.
+  #
+  # sender    - The User sending the message.
+  # recipient - The User receiving the message.
+  #
+  # Returns the delivered Message.
+  def deliver!(sender: nil, recipient: nil)
+    raise ArgumentError, "both sender and recipient must be specified" unless sender && recipient
+  end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe Message do
-  describe '.deliver' do
-    let(:message) { Message.new(body: 'Hello, there!') }
+  describe '.deliver!' do
+    let(:message)   { Message.new(body: 'Hello, there!') }
+    let(:sender)    { User.new }
+    let(:recipient) { User.new }
     
     context 'when both the sender and recipient are specified' do
       it 'should set the sender of the message'
@@ -13,15 +15,21 @@ describe Message do
     end
 
     context 'when neither the sender or recipient is specified' do
-      it 'should raise an error'
+      it 'should raise an error' do
+        expect { message.deliver! }.to raise_error ArgumentError
+      end
     end
 
     context 'when only the sender is specified' do
-      it 'should raise an error'
+      it 'should raise an error' do
+        expect { message.deliver! sender: sender }.to raise_error ArgumentError
+      end   
     end
     
     context 'when only the recipient is specified' do
-      it 'should raise an error'
+      it 'should raise an error' do
+        expect { message.deliver! recipient: recipient }.to raise_error ArgumentError
+      end
     end
   end
 end
