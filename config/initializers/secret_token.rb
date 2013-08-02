b68c1290323de5c96d912239b347e38b3cf19df2
Matt Buck <github@matthewbuck.com>
2013-08-02 05:30:01 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmployeePortal::Application.config.secret_token = '0e41725555058ecbdb32ddb0910475cd252e7d500f65da5039463a644267301954f9695faf902755e1cc074587aba16fdc9524257ef53a00d9f5de3496099fd2'
